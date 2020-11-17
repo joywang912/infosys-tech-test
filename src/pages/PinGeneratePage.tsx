841464901a0cf20f083dcb44a29c8e6860a54968
Jue Wang <joywang912@hotmail.com>
2020-11-17 20:35:44 +1300

    feat: add generate pin function
    PinGeneratePage add generate pin function when click generate button

--- src/pages/PinGeneratePage.tsx
+++ src/pages/PinGeneratePage.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../components/Input";
 import { Button, PrimaryButton } from "../components/Button";
+import { getPinList } from "../utils/PinGenerator";
 
 const PinGeneratePage = () => {
+  const [pins, setPins] = useState<number[][]>([[], [], [], [], []]);
+
+  const generatePins = () => {
+    const newPins = getPinList();
+    setPins(newPins);
+  };
   return (
     <div>
-      <div>
-        <Input readOnly size={4} />
+      <div className="align-center">
+        {pins.map((pin, idx) => {
+          return <Input key={idx} readOnly size={4} value={pin.join("")} />;
+        })}
       </div>
-      <Button>Generate</Button>
-      <PrimaryButton>Save</PrimaryButton>
+      <div className="row">
+        <div className="col">
+          <Button onClick={() => generatePins()}>Generate</Button>
+        </div>
+        <div className="col">
+          <PrimaryButton>Save</PrimaryButton>
+        </div>
+      </div>
     </div>
   );
 };
